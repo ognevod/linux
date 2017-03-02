c7eae9d68bbed46aff93c1b063f15f7c09cb743d
Anton Leontiev <aleontiev@elvees.com>
2017-03-02 09:50:41 +0300

    [media] avico: Add license notice to the headers
    Change-Id: Iabb331e0d1cc21720e8efd560612e84c1e04ec5d
    Signed-off-by: Anton Leontiev <aleontiev@elvees.com>

--- drivers/media/platform/avico/avico-debug.c
+++ drivers/media/platform/avico/avico-debug.c
@@ -1,8 +1,15 @@
 /*
- * Avico driver
+ * ELVEES Avico (a.k.a. VELcore-01) driver - Debug helpers
  *
  * Copyright 2015 ELVEES NeoTek JSC
+ * Copyright 2017 RnD Center "ELVEES", JSC
+ *
  * Author: Anton Leontiev <aleontiev@elvees.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  */
 
 #include <linux/types.h>
--- drivers/media/platform/avico/avico-driver.c
+++ drivers/media/platform/avico/avico-driver.c
@@ -1,8 +1,16 @@
 /*
- * Avico driver
+ * ELVEES Avico (a.k.a. VELcore-01) driver
  *
  * Copyright 2015 ELVEES NeoTek JSC
- * Author: Anton Leontiev <aleontiev@elvees.com>
+ * Copyright 2017 RnD Center "ELVEES", JSC
+ *
+ * Authors: Anton Leontiev <aleontiev@elvees.com>
+ *          Vasiliy Zasukhin <vzasukhin@elvees.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
